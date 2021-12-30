48dc2be1d1dff841355d7f74bd5cf9839783a602
jericogantuangco <jericogantuangco200@gmail.com>
2021-12-30 16:57:38 +0800

    added login data validation

--- app/src/main/java/com/example/travelblog/LoginActivity.kt
+++ app/src/main/java/com/example/travelblog/LoginActivity.kt
@@ -1,15 +1,66 @@
 package com.example.travelblog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.travelblog.databinding.ActivityMainBinding
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.travelblog.databinding.ActivityLoginBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.loginButton.setOnClickListener { onLoginClicked() }
+        binding.textUsernameLayout.editText?.addTextChangedListener(createTextWatcher(binding.textUsernameLayout))
+        binding.textPasswordInput.editText?.addTextChangedListener(createTextWatcher(binding.textPasswordInput))
+    }
+
+    private fun onLoginClicked(){
+        val username : String = binding.textUsernameLayout.editText?.text.toString()
+        val password : String = binding.textPasswordInput.editText?.text.toString()
+
+        if ( username.isEmpty() ) {
+            binding.textUsernameLayout.error = "Username must not be empty."
+        }
+
+        if ( password.isEmpty() ) {
+            binding.textPasswordInput.error = "Password must not be empty."
+        }
+
+        if (username != "admin" || password != "admin") {
+            showErrorDialog()
+        }
 
 
     }
+
+    private fun createTextWatcher(textInputLayout : TextInputLayout) : TextWatcher {
+        return object : TextWatcher {
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                textInputLayout.error = null
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // Meant to be blank since implementation not needed
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // Meant to be blank since implementation not needed
+            }
+        }
+    }
+
+    private fun showErrorDialog(){
+        AlertDialog.Builder(this)
+            .setTitle("Login Failed")
+            .setMessage("Username or Password is incorrect. Please try again.")
+            .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+            .show()
+    }
+
 }
